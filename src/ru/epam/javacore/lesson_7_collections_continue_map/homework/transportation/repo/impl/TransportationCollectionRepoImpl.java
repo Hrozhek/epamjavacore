e072fc888ee9c6d2bb878c91ce8d1ae4096dd0cd
DmitryYusupov <Dmitry_Yusupov@epam.com>
2019-12-17 13:54:27 +0300

    Made homework after 7-x lesson

---
+++ src/ru/epam/javacore/lesson_7_collections_continue_map/homework/transportation/repo/impl/TransportationCollectionRepoImpl.java
@@ -0,0 +1,22 @@
+package ru.epam.javacore.lesson_7_collections_continue_map.homework.transportation.repo.impl;
+
+import ru.epam.javacore.lesson_6_collections.homework.transportation.domain.Transportation;
+import ru.epam.javacore.lesson_6_collections.homework.transportation.repo.TransportationRepo;
+
+public class TransportationCollectionRepoImpl implements TransportationRepo {
+
+  @Override
+  public void add(Transportation transportation) {
+
+  }
+
+  @Override
+  public Transportation getById(long id) {
+    return null;
+  }
+
+  @Override
+  public boolean deleteById(long id) {
+    return false;
+  }
+}
