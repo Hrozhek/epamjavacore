9e81dc07250528e00ca1a4c5606f1f6f84b248d4
DmitryYusupov <Dmitry_Yusupov@epam.com>
2020-01-21 15:37:34 +0300

    -Added homework for lesson 16

---
+++ src/main/java/ru/epam/javacore/lesson_16_concurrency/homework/storage/initor/multithread/CarrierParser.java
@@ -0,0 +1,64 @@
+package ru.epam.javacore.lesson_16_concurrency.homework.storage.initor.multithread;
+
+import ru.epam.javacore.lesson_16_concurrency.homework.cargo.domain.Cargo;
+import ru.epam.javacore.lesson_16_concurrency.homework.carrier.domain.Carrier;
+import ru.epam.javacore.lesson_16_concurrency.homework.common.solutions.utils.FileUtils;
+import ru.epam.javacore.lesson_16_concurrency.homework.common.solutions.utils.xml.sax.XmlSaxUtils;
+import ru.epam.javacore.lesson_16_concurrency.homework.storage.initor.fileinitor.xml.sax.SaxHandler;
+import ru.epam.javacore.lesson_16_concurrency.homework.storage.initor.fileinitor.xml.sax.XmlSaxFileDataInitor;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+import javax.xml.parsers.SAXParser;
+
+public class CarrierParser implements Runnable{
+
+  private static final String FILE = "/ru/epam/javacore/lesson_16_concurrency/initdata/xmldata.xml";
+
+  private volatile boolean hasError = false;
+
+  private Map<String, Carrier> carrierMap;
+
+  @Override
+  public void run() {
+    File file = null;
+    try {
+      file = getFileWithInitData();
+      Map<String, Carrier> carrierMap = parseCarriersFromFile(file);
+      setCarrierMap(carrierMap);
+    } catch (Exception e) {
+      e.printStackTrace();
+      hasError = true;
+    } finally {
+      if (file != null) {
+        file.delete();
+      }
+    }
+  }
+
+  private Map<String, Carrier> parseCarriersFromFile(File file) throws Exception {
+    SAXParser parser = XmlSaxUtils.getParser();
+    SaxHandler saxHandler = new SaxHandler();
+    parser.parse(file, saxHandler);
+    return saxHandler.getCarrierMap();
+  }
+
+  private File getFileWithInitData() throws IOException {
+    return FileUtils
+        .createFileFromResource(
+            XmlSaxFileDataInitor.class, "init-data", "lesson", FILE);
+  }
+
+  public boolean isHasError() {
+    return hasError;
+  }
+
+  private synchronized void setCarrierMap(Map<String, Carrier> carrierMap) {
+    this.carrierMap = carrierMap;
+  }
+
+  public Map<String, Carrier> getCarrierMap() {
+    return this.carrierMap;
+  }
+}
