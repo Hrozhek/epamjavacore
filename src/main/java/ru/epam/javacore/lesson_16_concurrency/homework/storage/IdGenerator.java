5289d6f35a946ad083e6415a52c6c5eb7f69855f
DmitryYusupov <Dmitry_Yusupov@epam.com>
2020-01-22 17:55:34 +0300

    Now generator is thread safe

--- src/main/java/ru/epam/javacore/lesson_16_concurrency/homework/storage/IdGenerator.java
+++ src/main/java/ru/epam/javacore/lesson_16_concurrency/homework/storage/IdGenerator.java
@@ -1,13 +1,15 @@
 package ru.epam.javacore.lesson_16_concurrency.homework.storage;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 public final class IdGenerator {
 
     private IdGenerator() {
     }
 
-    private static Long id = 0L;
+    private static AtomicLong idGenerator = new AtomicLong(0);
 
     public static Long generateId() {
-        return ++id;
+        return idGenerator.incrementAndGet();
     }
 }
